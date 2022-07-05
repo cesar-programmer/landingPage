6530bbc941dc880fc200c531ec4086122251c80d
Cesar <CesarCordova.developer@gmail.com>
2022-07-05 07:32:49 -0700

    Updates

---
+++ assets/main.js
@@ -0,0 +1,47 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-console */
+const API = 'https://poke-info-api.p.rapidapi.com/pokemons';
+
+const content = null || document.getElementById('content');
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '30e295e8cdmsh78dced76fff328bp141b4djsndfac3d77b95f',
+    'X-RapidAPI-Host': 'poke-info-api.p.rapidapi.com',
+  },
+};
+
+async function fetchPokemons(urlAPI) {
+  const response = await fetch(urlAPI, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => { // esta es una funcion anonima auto ejecutable se ejecuta eela sola
+  try {
+    const pokemons = await fetchPokemons(API);
+    let view = `
+    ${pokemons.map((pokemon) => `
+    <div
+    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+    <img src="${pokemon.iconImg}" alt="${pokemon.iconImg}" class="w-full">
+    </div>
+    <div class="mt-4 flex justify-between">
+      <h3 class="text-sm text-gray-700">
+      <span aria-hidden="true" class="absolute inset-0"></span>
+      ${pokemon.name}
+      <div class="mt-4 flex justify-between">
+      <h3 class="text-sm text-gray-700">
+      <span aria-hidden="true" class="absolute inset-0"></span>
+      ${pokemon.types}
+      </h3>
+    </div>
+  </div>
+    `).slice(0, 20).join('')}
+    `;
+    content.innerHTML = view;
+  } catch (err) {
+    console.error(err);
+  }
+})();
