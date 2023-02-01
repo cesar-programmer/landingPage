684fd9bf92151c31e83158b5493f17cd6d7ec603
Cesar <CesarCordova.developer@gmail.com>
2023-01-31 19:32:49 -0800

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-console */
-const API = 'https://poke-info-api.p.rapidapi.com/pokemons';
+const API = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&sortBy=ranking&sortOrder=asc';
 
 const content = null || document.getElementById('content');
 
@@ -8,11 +8,11 @@ const options = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': '30e295e8cdmsh78dced76fff328bp141b4djsndfac3d77b95f',
-    'X-RapidAPI-Host': 'poke-info-api.p.rapidapi.com',
+    'X-RapidAPI-Host': 'anime-db.p.rapidapi.com',
   },
 };
 
-async function fetchPokemons(urlAPI) {
+async function fetchAnimes(urlAPI) {
   const response = await fetch(urlAPI, options);
   const data = await response.json();
   return data;
@@ -20,22 +20,27 @@ async function fetchPokemons(urlAPI) {
 
 (async () => { // esta es una funcion anonima auto ejecutable se ejecuta eela sola
   try {
-    const pokemons = await fetchPokemons(API);
+    const animes = await fetchAnimes(API);
     let view = `
-    ${pokemons.map((pokemon) => `
+    ${animes.data.map((anime) => `
     <div
     class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-    <img src="${pokemon.iconImg}" alt="${pokemon.iconImg}" class="w-full">
+    <img src="${anime.image}" alt="${anime.image}" class="w-full">
     </div>
     <div class="mt-4 flex justify-between">
       <h3 class="text-sm text-gray-700">
       <span aria-hidden="true" class="absolute inset-0"></span>
-      ${pokemon.name}
+      title ${anime.title}
       <div class="mt-4 flex justify-between">
       <h3 class="text-sm text-gray-700">
       <span aria-hidden="true" class="absolute inset-0"></span>
-      ${pokemon.types}
+      Ranking ${anime.ranking}
+      <div class="mt-4 flex justify-between">
+      <h3 class="text-sm text-gray-700">
+      <span aria-hidden="true" class="absolute inset-0"></span>
+      Episodes ${anime.episodes}
       </h3>
+      </div>
     </div>
   </div>
     `).slice(0, 20).join('')}
